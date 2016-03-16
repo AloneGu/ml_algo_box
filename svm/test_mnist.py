61b24d98c0bd7dd7176931ab6d297a4dd043a699
Wei <wei.gu@percolata.com>
2016-03-16 15:34:14 +0800

    Added time cost

--- svm/test_mnist.py
+++ svm/test_mnist.py
@@ -1,5 +1,6 @@
 from sklearn.svm import SVC
 from sklearn import metrics
+import time
 
 def get_mnist_data():
     import gzip,pickle
@@ -15,8 +16,11 @@ def get_mnist_data():
 train_x, train_y, test_x, test_y = get_mnist_data()
 print 'get data'
 model = SVC(kernel='rbf', probability=True)
+# takes really long time
+start_time = time.time()
 model.fit(train_x, train_y)
+print 'time cost', time.time() - start_time
 predict = model.predict(test_x)
 print 'get model'
 print metrics.accuracy_score(test_y,predict)
-print metrics.recall_score(test_y,predict)
+#print metrics.recall_score(test_y,predict)
